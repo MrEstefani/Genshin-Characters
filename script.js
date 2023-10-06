29a1c556b74593dea0af6583fc52fc1e313d2930
MrEstefani <estefanimarquesrosa@gmail.com>
2023-10-06 15:51:15 -0300

    novo bg

--- script.js
+++ script.js
@@ -17,76 +17,90 @@ var name = document.getElementById('name')
             gen = 'Boy'
             if(fvisio[0].checked){
                 visio = 'Pyro'
-                document.body.style.background = '#F20505'
                 img.setAttribute('src','photo/bennett.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#F20505'
                 name.innerHTML = `Bennett`
             }else if (fvisio[1].checked){
                 visio = 'Anemo'
-                document.body.style.background = '#3EA086'
                 img.setAttribute('src','photo/venti.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#3EA086'
                 name.innerHTML = `Venti`
             }else if(fvisio[2].checked){
                 visio = 'Cryo'
-                document.body.style.background = '#639CBF'
                 img.setAttribute('src','photo/chongyun.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#639CBF'
                 name.innerHTML = `Choungyun`
             }else if (fvisio[3].checked){
                 visio = 'Geo'
-                document.body.style.background = '#F2D857'
                 img.setAttribute('src','photo/itto.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#F2D857'
                 name.innerHTML = `Itto`
             }else if (fvisio[4].checked){
                 visio = 'Dendro'
-                document.body.style.background = '#9CD9AB'
                 img.setAttribute('src','photo/beizou.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#9CD9AB'
                 name.innerHTML = `Beizou`
             }else if (fvisio[5].checked){
                 visio = 'Eletro'
-                document.body.style.background = '#545ABF'
                 img.setAttribute('src','photo/cyno.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#545ABF'
                 name.innerHTML = `Cyno`
             }else{
                 visio = 'Hydro'
-                document.body.style.background = '#3F3F81'
                 img.setAttribute('src','photo/kamisatoayato.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#3F3F81'
                 name.innerHTML = `Kamisato Ayato`
             }
         }else if(fgen[1].checked){
             gen = 'Girl'
             if(fvisio[0].checked){
                 visio = 'Pyro'
-                document.body.style.background = '#C13E2E'
                 img.setAttribute('src','photo/yoimiya.png',)
+                document.body.style.transition = '1s'
+                document.body.style.background = '#C13E2E'
                 name.innerHTML = `Yoimiya`
             }else if (fvisio[1].checked){
                 visio = 'Anemo'
-                document.body.style.background = '#518C77'
                 img.setAttribute('src','photo/sucrose.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#518C77'
                 name.innerHTML = `Sucrose`
             }else if(fvisio[2].checked){
                 visio = 'Cryo'
-                document.body.style.background = '#659DBF'
                 img.setAttribute('src','photo/ganyu.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#659DBF'
                 name.innerHTML = `Ganyu`
             }else if (fvisio[3].checked){
                 visio = 'Geo'
-                document.body.style.background = '#DBB65F'
                 img.setAttribute('src','photo/ningguang.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#DBB65F'
                 name.innerHTML = `Ningguang`
             }else if (fvisio[4].checked){
                 visio = 'Dendro'
-                document.body.style.background = '#9CBF4E'
                 img.setAttribute('src','photo/nahida.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#9CBF4E'
                 name.innerHTML = `Nahida`
             }else if (fvisio[5].checked){
                 visio = 'Eletro'
-                document.body.style.background = '#731F26'
                 img.setAttribute('src','photo/yae.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#731F26'
                 name.innerHTML = `Yae Miko`
             }else{
                 visio = 'Hydro'
-                document.body.style.background = '#AD97D3x'
                 img.setAttribute('src','photo/sangonomiya.png')
+                document.body.style.transition = '1s'
+                document.body.style.background = '#AD97D3x'
                 name.innerHTML = `Sangonomiya Kokomi`
             }
         }
